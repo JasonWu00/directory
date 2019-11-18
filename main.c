1f26f5e8e59fb3bfac2593d981d35861c4996bc7
JasonWu00 <zwu00@stuy.edu>
2019-11-18 10:46:55 -0500

    work

--- main.c
+++ main.c
@@ -3,12 +3,14 @@
 #include <dirent.h>
 #include <errno.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
 
 int main() {
   printf("The c file will be going through the directory it is in.\n");
 
   DIR * directory;
-  directory = opendir("/mnt/c/Users/jason/Desktop/[DATA EXPUNGED 2]/WoRk/Systems/directory");
+  directory = opendir(".");
   printf("Error if any: %i: %s\n", errno, strerror(errno));
   struct dirent *currentFile;
   int counter = 0;
@@ -17,7 +19,22 @@ int main() {
     currentFile = readdir(directory);
     if (currentFile != NULL) {
       printf("Returning metadata on file number %i:\n", counter);
+      printf("Error, if any: %i: %s\n", errno, strerror(errno));
       printf("File name: %s\n", currentFile->d_name);
+      struct stat filestat;
+      stat(currentFile->d_name, &filestat);
+      printf("File size: %li bytes\n", filestat.st_size);
+      char filetype[25];
+      if (currentFile->d_type == DT_REG) {
+        strcpy(filetype, "Normal file");
+      }
+      else if (currentFile->d_type == DT_DIR) {
+        strcpy(filetype, "Directory");
+      }
+      else {
+        strcpy(filetype, "other");
+      }
+      printf("File type: %d: %s\n", currentFile->d_type, filetype);
       counter++;
       printf("\n");
     }
